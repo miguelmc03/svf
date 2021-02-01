76b58536d3eaa45994978ea560233633b963a4c0
yormanpineda <yormanpineda2@gmail.com>
2021-02-01 12:17:44 -0400

    modulo de gastos excel y ventas apoyo analytic

--- 3mit_ventas_apoyo_analytic/__manifest__.py
+++ 3mit_ventas_apoyo_analytic/__manifest__.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Menu para Cargas y ejecutar Gastos",
-    'summary': "Menu Carga Gastos",
+    'name': "Ajuste de cuenta analitica de ventas-Factura",
+    'summary': "Ajuste de cuentas analiticas por tema apoyo pos venta",
     'description': "Elaborado por: Ing Yorman Pineda",
     'version': '1.0',
     'author': '3mit',
     'category': 'Tools',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'hr_expense', 'sale'],
+    'depends': ['base', 'account'],
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
-        'wizard/wizard_excel.xml',
-        'views/marcas_herencias.xml',
+
     ],
     'demo': [
     ],
