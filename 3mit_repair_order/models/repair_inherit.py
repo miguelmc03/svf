541213dba6f1ec86324cbb37e9897e07428d7af5
yormanpineda <yormanpineda2@gmail.com>
2021-02-02 09:50:01 -0400

    cambio precios en orden de reparacion

--- 3mit_repair_order/models/repair_inherit.py
+++ 3mit_repair_order/models/repair_inherit.py
@@ -42,16 +42,36 @@ class RepairLineInherit(models.Model):
             self.location_id = self.env['stock.location'].search([('usage', '=', 'production')], limit=1).id
             self.location_dest_id = self.env['stock.location'].search([('scrap_location', '=', True)], limit=1).id
 
+    # LÓGICA PARA TOMAR EL PRECIO POR UNIDAD DEL PRODUCTO Y REALIZA BYPASS INDEPENDIENTEMENTE DE SI ES TYPE REMOVE O NO:
     @api.onchange('product_uom')
     def _onchange_product_uom(self):
-        price = self.product_id.standard_price
-        if price is False:
-            warning = {
-                'title': _('No se encontró una línea de lista de precios válida.'),
-                'message':
-                    _(
-                        "No se pudo encontrar una línea de lista de precios que coincida con este producto y "
-                        "la cantidad. \nDebe cambiar el producto, la cantidad o la lista de precios.")}
-            return {'warning': warning}
-        else:
-            self.price_unit = price
+        partner = self.repair_id.partner_id
+        pricelist = self.repair_id.pricelist_id
+        if pricelist and self.product_id and self.type:
+            price = pricelist.get_product_price(self.product_id, self.product_uom_qty, partner,
+                                                uom_id=self.product_uom.id)
+            if price is False:
+                warning = {
+                    'title': _('No se encontró una línea de lista de precios válida.'),
+                    'message':
+                        _(
+                            "No se pudo encontrar una línea de lista de precios que coincida con este producto y "
+                            "la cantidad. \nDebe cambiar el producto, la cantidad o la lista de precios.")}
+                return {'warning': warning}
+            else:
+                self.price_unit = price
+
+    # LÓGICA PARA TOMAR EL COSTE DEL PRODUCTO
+    # @api.onchange('product_uom')
+    # def _onchange_product_uom(self):
+        # price = self.product_id.standard_price
+        # if price is False:
+            # warning = {
+                # 'title': _('No se encontró una línea de lista de precios válida.'),
+                # 'message':
+                    # _(
+                        # "No se pudo encontrar una línea de lista de precios que coincida con este producto y "
+                        # "la cantidad. \nDebe cambiar el producto, la cantidad o la lista de precios.")}
+            # return {'warning': warning}
+        # else:
+            # self.price_unit = price
